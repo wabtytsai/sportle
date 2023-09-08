af74826ad5ffc6cf97d2280d412ea78b6fb12c08
Edward Tsai <edward.tsai.0091+github@gmail.com>
2023-09-07 21:36:05 -0700

    refactor: move puzzle api into client size

--- client/src/pages/sports-grid/grid.js
+++ client/src/pages/sports-grid/grid.js
@@ -1,42 +1,19 @@
 'use client'
 
 import Table from 'react-bootstrap/Table';
-import Spinner from 'react-bootstrap/Spinner';
-
-import React, { useEffect, useState } from 'react';
-
+import React, { useMemo } from 'react';
 import PlayerInput from './PlayerInput';
 import TeamLogo from './TeamLogo';
-
+import { getPuzzle as getBasketballPuzzle } from './games/basketball';
 import * as constants from '../../utils/constants';
 
 export default function Grid() {
     const sport = 'NBA';
 
-    const dataUrl = constants.routes[sport];
     const logoSrc = constants.logos[sport];
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        async function fetchGrid() {
-            const response = await fetch(dataUrl);
-            const json = await response.json();
-    
-            setData(json);
-            setIsLoading(false);
-        }
-
-        fetchGrid();
-    }, [])
-
-    if (isLoading) {
-        return(
-            <div>
-                <Spinner animation="border" role="status"/>
-            </div>)
-    }
+    // TODO(tseddie): generalize this
+    const data = useMemo(getBasketballPuzzle, sport); 
 
     const tableHeaders = data.cols.map(team => (<TeamLogo key={team.Name} source={team.Logo}/>));
     const tableRows = [];
